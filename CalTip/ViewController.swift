7ca5f6e8bb047678b818812e17a58ef0124c3f38
Gulesh <gshukla@haverford.edu>
2020-04-12 13:18:43 -0400

    Initial Commit

---
+++ CalTip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CalTip
+//
+//  Created by Gulesh Shukla on 4/12/20.
+//  Copyright © 2020 Gulesh Shukla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
